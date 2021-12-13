5733375debe81a1b2aede63efb259c576f790fcf
Carafassi Samuele <samuele.carafassi@studio.unibo.it>
2021-12-13 13:05:46 +0100

    completed exercise 2-Exceptions

--- 2-Exceptions/FixedSizeQueue.cs
+++ 2-Exceptions/FixedSizeQueue.cs
@@ -6,28 +6,34 @@ namespace Exceptions
         private object[] _items;
         private int _firstIndex = 0;
         private int _lastIndex = 0;
-        
+
         public FixedSizeQueue(uint capacity)
         {
             Capacity = capacity;
             _items = new object[capacity];
         }
-        
+
         public uint Capacity { get; }
 
         public uint Count => (uint) (_lastIndex - _firstIndex);
 
         public object GetFirst()
         {
-            // TODO ensure objects can only be retrieve if the item is queue is not empty
+            if (Count < 1)
+            {
+                throw new EmptyQueueException();
+            }
             var first = _items[_firstIndex % Capacity];
             _firstIndex++;
             return first;
         }
-        
+
         public void AddLast(object item)
         {
-            // TODO ensure objects can only be inserted if the item is queue is not full
+            if (Count == Capacity)
+            {
+                throw new FullQueueException();
+            }
             _items[_lastIndex % Capacity] = item;
             _lastIndex++;
         }
